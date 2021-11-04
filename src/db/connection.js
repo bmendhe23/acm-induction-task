797b04fedaf44677574602fe3425bba512775946
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-11-05 03:30:02 +0530

    env variable added

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/regisUsers", { useNewUrlParser: true})
+mongoose.connect("mongodb://localhost:27017/"+process.env.DB_NAME, { useNewUrlParser: true})
 .then( () => {
     console.log("Database Connection Established");
 })
